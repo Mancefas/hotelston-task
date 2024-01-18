3489481802ae7b01b5b36843bacae6c50e808645
Mantvydas Liobė <mantvydasliobe@gmail.com>
2024-01-18 10:27:23 +0200

    api endpoints handlers top get locations list and lonmg forecast for city

---
+++ src/helpers/getLongForecast.ts
@@ -0,0 +1,13 @@
+'use server'
+
+export async function getLongForecast( place : string) {
+    //the API point should come from .env, but for this project I have hardcoded it, because it has no authorization and this component lives in server
+    const res = await fetch(`https://api.meteo.lt/v1/places/${place}/forecasts/long-term`);
+
+if (!res.ok) {
+    // can be done with CSS using capitalize, but this is in server and it does not add css to it
+    throw new Error(`Failed to fetch long term forecast for ${place[0].toUpperCase() + place.slice(1)} `);
+}
+
+return res.json();
+}
