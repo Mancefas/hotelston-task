82963add04fdd3911d08519657698d5332a98bcb
Mantvydas Liobė <mantvydasliobe@gmail.com>
2024-01-19 12:35:17 +0200

    feature: sample data, types for weather

---
+++ src/store/dataSample.ts
@@ -0,0 +1,121 @@
+export const object = {
+    "place": {
+        "code": "vilnius",
+        "name": "Vilnius",
+        "administrativeDivision": "Vilniaus miesto savivaldybė",
+        "country": "Lietuva",
+        "countryCode": "LT",
+        "coordinates": {
+            "latitude": 54.687046,
+            "longitude": 25.282911
+        }
+    },
+    "forecastType": "long-term",
+    "forecastCreationTimeUtc": "2024-01-18 08:00:51",
+    "forecastTimestamps": [
+        {
+            "forecastTimeUtc": "2024-01-18 08:00:00",
+            "airTemperature": -3.2,
+            "feelsLikeTemperature": -7.3,
+            "windSpeed": 3,
+            "windGust": 6,
+            "windDirection": 184,
+            "cloudCover": 100,
+            "seaLevelPressure": 988,
+            "relativeHumidity": 93,
+            "totalPrecipitation": 0,
+            "conditionCode": "cloudy"
+        },
+        {
+            "forecastTimeUtc": "2024-01-18 09:00:00",
+            "airTemperature": -2.5,
+            "feelsLikeTemperature": -5.4,
+            "windSpeed": 2,
+            "windGust": 5,
+            "windDirection": 204,
+            "cloudCover": 100,
+            "seaLevelPressure": 988,
+            "relativeHumidity": 93,
+            "totalPrecipitation": 0,
+            "conditionCode": "cloudy"
+        },
+        {
+            "forecastTimeUtc": "2024-01-18 10:00:00",
+            "airTemperature": -2,
+            "feelsLikeTemperature": -5.9,
+            "windSpeed": 3,
+            "windGust": 5,
+            "windDirection": 243,
+            "cloudCover": 100,
+            "seaLevelPressure": 989,
+            "relativeHumidity": 93,
+            "totalPrecipitation": 0,
+            "conditionCode": "cloudy"
+        },
+        {
+            "forecastTimeUtc": "2024-01-18 11:00:00",
+            "airTemperature": -1.4,
+            "feelsLikeTemperature": -6,
+            "windSpeed": 4,
+            "windGust": 7,
+            "windDirection": 280,
+            "cloudCover": 100,
+            "seaLevelPressure": 989,
+            "relativeHumidity": 91,
+            "totalPrecipitation": 0,
+            "conditionCode": "cloudy"
+        },
+        {
+            "forecastTimeUtc": "2024-01-18 12:00:00",
+            "airTemperature": -1.5,
+            "feelsLikeTemperature": -6.1,
+            "windSpeed": 4,
+            "windGust": 8,
+            "windDirection": 290,
+            "cloudCover": 100,
+            "seaLevelPressure": 990,
+            "relativeHumidity": 88,
+            "totalPrecipitation": 0,
+            "conditionCode": "cloudy"
+        },
+        {
+            "forecastTimeUtc": "2024-01-18 13:00:00",
+            "airTemperature": -1.6,
+            "feelsLikeTemperature": -6.2,
+            "windSpeed": 4,
+            "windGust": 8,
+            "windDirection": 290,
+            "cloudCover": 100,
+            "seaLevelPressure": 991,
+            "relativeHumidity": 87,
+            "totalPrecipitation": 0,
+            "conditionCode": "cloudy"
+        },
+        {
+            "forecastTimeUtc": "2024-01-18 14:00:00",
+            "airTemperature": -1.9,
+            "feelsLikeTemperature": -5.8,
+            "windSpeed": 3,
+            "windGust": 7,
+            "windDirection": 280,
+            "cloudCover": 96,
+            "seaLevelPressure": 992,
+            "relativeHumidity": 88,
+            "totalPrecipitation": 0,
+            "conditionCode": "cloudy"
+        },
+        {
+            "forecastTimeUtc": "2024-01-18 15:00:00",
+            "airTemperature": -3.7,
+            "feelsLikeTemperature": -6.8,
+            "windSpeed": 2,
+            "windGust": 5,
+            "windDirection": 259,
+            "cloudCover": 77,
+            "seaLevelPressure": 993,
+            "relativeHumidity": 91,
+            "totalPrecipitation": 0,
+            "conditionCode": "cloudy"
+        },
+    ]
+}
