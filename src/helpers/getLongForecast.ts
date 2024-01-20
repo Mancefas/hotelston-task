8e020ba4d1bd93044bf565166998d5358a117006
Mantvydas Liobė <mantvydasliobe@gmail.com>
2024-01-20 16:03:47 +0200

    feature: added revalidation for fetch

--- src/helpers/getLongForecast.ts
+++ src/helpers/getLongForecast.ts
@@ -2,10 +2,10 @@
 
 export async function getLongForecast( place : string) {
     //the API point should come from .env, but for this project I have hardcoded it, because it has no authorization and this component lives in server
-    const res = await fetch(`https://api.meteo.lt/v1/places/${place}/forecasts/long-term`);
+    // cashing the results, but revalidating it evey hour
+    const res = await fetch(`https://api.meteo.lt/v1/places/${place}/forecasts/long-term`, { next: { revalidate: 3600 }});
 
 if (!res.ok) {
-    // can be done with CSS using capitalize, but this is in server and it does not add css to it
     throw new Error(`Failed to fetch forecast for ${place[0].toUpperCase() + place.slice(1)} `);
 }
 
