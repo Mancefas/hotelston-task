84ea18a84a7963bfb377ef9bb3a2ae578b5e1a2f
Mantvydas Liobė <mantvydasliobe@gmail.com>
2024-01-20 10:49:29 +0200

    featuer: more tests

--- src/components/organisms/SelectCitySection/SelectCitySection.test.tsx
+++ src/components/organisms/SelectCitySection/SelectCitySection.test.tsx
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import SelectCitySection from './SelectCitySection';
 
 describe('SelectCitySection', () => {
@@ -7,5 +8,18 @@ describe('SelectCitySection', () => {
         render(<SelectCitySection />);
         const component = screen.getByTestId('select-city-section');
         expect(component).toBeInTheDocument();
+    });
+    it('when component is rendered the button is disabled', () => {
+        render(<SelectCitySection />);
+        const button = screen.getByRole('button', { name: 'search' });
+        expect(button).toBeDisabled();
+    });
+    it('when input is nonsence it get error state', async () => {
+        render(<SelectCitySection />);
+        const input = screen.getByRole('textbox');
+        await userEvent.type(input, 'ahsdvj');
+        const errorClass = document.querySelector('.input--error');
+
+        expect(errorClass).toBeInTheDocument();
     });
 });
