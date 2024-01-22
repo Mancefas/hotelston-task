f4f638c8839b12cb3d5f5be1da70dbf615d321fa
Mantvydas Liobė <mantvydasliobe@gmail.com>
2024-01-22 11:26:35 +0200

    feature: linting file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Hotelston front-end assigment',
-  description: 'create next app project for Hotelston front-end assigment',
-}
+    title: 'Hotelston front-end assigment',
+    description: 'create next app project for Hotelston front-end assigment',
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
